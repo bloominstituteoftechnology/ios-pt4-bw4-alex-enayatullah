421fa3f03f06bab3e7aa5b67f3f5a090b0a01ab5
Alex Shillingford <alex.shillingford@icloud.com>
2020-06-17 19:09:08 -0600

    Create Xcode Project

---
+++ GameSafe/GameSafe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GameSafe
+//
+//  Created by Alex Shillingford on 6/17/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
