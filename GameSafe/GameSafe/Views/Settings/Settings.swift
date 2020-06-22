54a34affe304841d598cb844cc549e257d5520e9
Alex Shillingford <alex.shillingford@icloud.com>
2020-06-21 22:24:25 -0600

    Make some changes to Settings view

--- GameSafe/GameSafe/Views/Settings/Settings.swift
+++ GameSafe/GameSafe/Views/Settings/Settings.swift
@@ -13,47 +13,50 @@ struct Settings: View {
     
     var body: some View {
         NavigationView {
-            ScrollView {
-                VStack(spacing: 1) {
-                    NavigationLink(destination: AssignMainContactView(contacts: self.$contacts)) {
-                        SettingsOptionRow(setting: "Main Contact")
+            ZStack {
+                ScrollView {
+                    VStack(spacing: 1) {
+                        NavigationLink(destination: AssignMainContactView(contacts: self.$contacts)) {
+                            SettingsOptionRow(setting: "Main Contact")
+                        }
+                        SettingsOptionRow(setting: "Backup Contacts")
                     }
-                    SettingsOptionRow(setting: "Backup Contacts")
-                }
-                .padding(.top, 1)
-                .padding(.bottom, 30)
-                
-                VStack(spacing: 1) {
-                    HStack {
-                        Text("Difficulty")
-                            .font(.title).bold()
-                        Spacer()
-                    }
-                    .frame(height: 50)
-                    .padding(.horizontal)
+                    .padding(.top, 1)
+                    .padding(.bottom, 30)
                     
-                    SettingsOptionRow(setting: "Easy")
-                    SettingsOptionRow(setting: "Medium")
-                    SettingsOptionRow(setting: "Hard")
-                }
-                .padding(.bottom, 30)
-                
-                VStack(spacing: 1) {
-                    HStack {
-                        Text("Resources")
-                            .font(.title).bold()
-                        Spacer()
+                    VStack(spacing: 1) {
+                        HStack {
+                            Text("Difficulty")
+                                .font(.title).bold()
+                            Spacer()
+                        }
+                        .frame(height: 50)
+                        .padding(.horizontal)
+                        
+                        SettingsOptionRow(setting: "Easy")
+                        SettingsOptionRow(setting: "Medium")
+                        SettingsOptionRow(setting: "Hard")
                     }
-                    .frame(height: 50)
-                    .padding(.horizontal)
+                    .padding(.bottom, 30)
                     
-                    SettingsOptionRow(setting: "Review Instructions")
+                    VStack(spacing: 1) {
+                        HStack {
+                            Text("Resources")
+                                .font(.title).bold()
+                            Spacer()
+                        }
+                        .frame(height: 50)
+                        .padding(.horizontal)
+                        
+                        SettingsOptionRow(setting: "Review Instructions")
+                    }
                 }
+                .background(Color(#colorLiteral(red: 0.955273211, green: 0.955273211, blue: 0.955273211, alpha: 0.9880136986)))
             }
-            .background(Color(#colorLiteral(red: 0.9561466575, green: 0.9561466575, blue: 0.9561466575, alpha: 1)))
-            .navigationBarTitle("Settings")
+            .edgesIgnoringSafeArea(.bottom)
+            .navigationBarTitle("Settings", displayMode: .automatic)
         }
-        .edgesIgnoringSafeArea(.bottom)
+        .edgesIgnoringSafeArea(.top)
     }
 }
 
