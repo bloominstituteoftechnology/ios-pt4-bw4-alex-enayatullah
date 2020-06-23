06c2f6afd16f74dffe6649920eb093190a724321
Enayatullah Naseri <enayatullahna@gmail.com>
2020-06-22 18:23:34 -0700

    Updated created signup function

--- GameSafe/GameSafe/Model Controller/GameSafeController.swift
+++ GameSafe/GameSafe/Model Controller/GameSafeController.swift
@@ -8,4 +8,58 @@
 
 import Foundation
 
+enum NetworkError: Error {
+    case noAuth
+    case badAuth
+    case otherError
+    case badData
+    case noDecode
+}
 
+struct HTTPMethod {
+    static let get = "GET"
+    static let put = "PUT"
+    static let post = "POST"
+    static let delete = "DELETE"
+}
+
+class GameSafeController {
+    
+    private let baseURL = URL(string: "https")!
+    
+    func signUp(username: String, password: String, completion: @escaping (Error?) -> Void = { _ in }) {
+        let signUpURL = baseURL.appendingPathComponent("register")
+        
+        var request = URLRequest(url: signUpURL)
+        request.httpMethod = HTTPMethod.post
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let userParams = ["username": username, "password": password] as [String: Any]
+        do {
+            let json = try JSONSerialization.data(withJSONObject: userParams, options: .prettyPrinted)
+            request.httpBody = json
+        } catch {
+            NSLog("Error encoding JSON")
+            return
+        }
+        URLSession.shared.dataTask(with: request) { _, response, error in
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                
+                completion(NSError(domain: "", code: response.statusCode, userInfo: nil))
+                return
+            }
+            if let error = error {
+                completion(error)
+                return
+            }
+            NSLog("Successfully signed up User")
+            
+            completion(nil)
+        } .resume()
+    }
+    
+    func signIn() {
+        
+    }
+}
