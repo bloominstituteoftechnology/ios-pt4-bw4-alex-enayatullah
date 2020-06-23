7770d875afc20e8aed7a0289a03a64562582239d
Enayatullah Naseri <enayatullahna@gmail.com>
2020-06-22 18:21:27 -0700

    Started model and core data.

---
+++ GameSafe/GameSafe/LSIGameSafe.m
@@ -0,0 +1,13 @@
+//
+//  LSIGameSafe.m
+//  GameSafe
+//
+//  Created by Enayatullah Naseri on 6/21/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "LSIGameSafe.h"
+
+@implementation LSIGameSafe
+
+@end
---
+++ GameSafe/GameSafe/Model Controller/GameSafeController.swift
@@ -0,0 +1,11 @@
+//
+//  GameSafeController.swift
+//  GameSafe
+//
+//  Created by Enayatullah Naseri on 6/21/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+
