048952cbfe9e7bf691a7a49a8f8e199e2d2efba1
Alex Shillingford <alex.shillingford@icloud.com>
2020-06-24 21:52:58 -0600

    Create MessagesComposeView

---
+++ GameSafe/GameSafe/Views/Library/LibrarySubViews/MessagesComposeView.swift
@@ -0,0 +1,21 @@
+//
+//  MessagesComposeView.swift
+//  GameSafe
+//
+//  Created by Alex Shillingford on 6/24/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import SwiftUI
+
+struct MessagesComposeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MessagesComposeView_Previews: PreviewProvider {
+    static var previews: some View {
+        MessagesComposeView()
+    }
+}
